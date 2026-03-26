254d46f946dde1cc3feefc87d4955699e065698d
101006 <melcy.francis@mactech.net.in>
2026-03-26 18:55:12 +0530

    IDFC

---
+++ GolLoanSecureGateWay/Model/ColendingybiRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoldLoanSecureGateWay.Model
+{
+    public class ColendingybiRequest
+    {
+        public class ColendingybiAgreementsignRequest
+        {
+            public string Document { get; set; }
+            public string Quotation_no { get; set; }
+            public string path { get; set; }
+        }
+
+    }
+}
---
+++ GolLoanSecureGateWay/Model/ColendingyubiResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoldLoanSecureGateWay.Model
+{
+    public class ColendingyubiResponse
+    {
+       public class ColendingybiAgreementsignResponse
+        {
+            public int result { get; set; }
+            public int flag { get; set; }
+            public int code { get; set; }
+        }
+    }
+}
